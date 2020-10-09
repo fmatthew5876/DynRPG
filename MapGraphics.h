b1865ac91c58ef17bd32d1329206e8c0ca127de8
Matthew Fioravante <fmatthew5876@gmail.com>
2020-10-09 17:29:12 -0400

    More objects in MapGraphics

--- MapGraphics.h
+++ MapGraphics.h
@@ -13,5 +13,8 @@ namespace RPG {
 			DStringPtr filename; //!< The filename of the tileset (again...)
 			Image *chipsetImage; //!< The chipset's image
 			Image *_unknown_10; //!< Some other image??
+			int _unknown_14;
+			int _unknown_18;
+			int* _unknown_1C;
 	};
 }
