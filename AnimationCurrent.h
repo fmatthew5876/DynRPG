dc224ba28fabc30a1c82b80b8fb701a06bdb67e7
Matthew Fioravante <fmatthew5876@gmail.com>
2020-10-12 11:55:30 -0400

    More 2k fixes

--- AnimationCurrent.h
+++ AnimationCurrent.h
@@ -23,11 +23,13 @@ namespace RPG {
 			Animation *anim; //!< Animation structure of the current battle animation. 0 if BattleCharSets
 			DStringPtr filename; //!< Filename of the animation that has been stored in memory
 			Image *image; //!< The actual image data of the battle animation stored in memory. Only use with monsters
+#if RPG_RT_ENGINE == 2003
 			Image *imageLarge; //!< The image data if it's a large battle animation, which has been stored in memory. Only use with monsters
+#endif
 			AnimationFlashEffect* targetFlashEffects; //<! For each frame, says what the target flash effect should be.
 			AnimationFlashEffect* screenFlashEffects; //<! For each frame, says what the screen flash effect should be.
-			AnimationShakeEffect* shakeEffects; //<! For each frame, says what the shake effect scope should be.
 #if RPG_RT_ENGINE == 2003
+			AnimationShakeEffect* shakeEffects; //<! For each frame, says what the shake effect scope should be.
 			bool isMirrored; //!< Is the animation currently mirrored?
 #endif
 
